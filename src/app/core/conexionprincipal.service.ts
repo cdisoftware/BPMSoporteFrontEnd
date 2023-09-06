6e5045dcae73e5051056a73d05d02ee59c03e7c0
williambernal1003 <84921661+williambernal1003@users.noreply.github.com>
2023-09-06 11:23:44 -0500

    Ajustes

--- src/app/core/conexionprincipal.service.ts
+++ src/app/core/conexionprincipal.service.ts
@@ -11,12 +11,15 @@ export class ConexionprincipalService {
     //ambiente de trabajo 1 desarrollo 2 produccion
     ambientedetrabajo: string = '2';
 
+    public url_DesarolloCDI = 'http://localhost:1004/BPMSoporteService/';
     public url_ProduccionCDI = 'http://190.147.38.91:1004/BPMSoporteService/';
 
 
     SeleccionAmbiente() {
         if (this.ambientedetrabajo == '2') {
             return this.url_ProduccionCDI;
+        }else if(this.ambientedetrabajo == '1'){
+            return this.url_DesarolloCDI;
         }
         else {
             return "Valida ambiente seleccionado";
